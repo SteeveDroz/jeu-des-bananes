d23a195ff9942b39e95ee807caab59ef60c3329f
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-09-07 16:22:09 +0200

    Création des fiches individuelles

--- js/compte.js
+++ js/compte.js
@@ -2,12 +2,11 @@ $(function() {
     const annees = [1, 2, 3, 4, 5, 6]
     $('.comptes').each(function(index) {
         const comptes = $(this)
-        const modele = $(this).find('tbody')
         annees.forEach(function(annee) {
-            const compteAnnee = modele.clone(true)
+            const compteAnnee = comptes.clone(true)
             compteAnnee.html(compteAnnee.html().replace('%annee', annee))
-            comptes.append(compteAnnee)
+            comptes.before(compteAnnee)
         })
-        modele.remove()
+        comptes.remove()
     })
 })
