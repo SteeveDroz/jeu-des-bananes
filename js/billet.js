6224d0fb37679f868cc2f5a035a1b399504b3517
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-09-09 09:40:06 +0200

    Ajout des billets

---
+++ js/billet.js
@@ -0,0 +1,24 @@
+const nombreBillets = 10
+$(function() {
+    $('#generer-billets').click(genererBillets)
+})
+const genererBillets = function() {
+    const valeur = $('#valeur-billets').val()
+    if (valeur > 0) {
+        const modele = $('#modele-billet')
+        const parent = modele.parent()
+        modele.detach()
+        parent.html('')
+        parent.append(modele)
+        for (let i = 0; i < nombreBillets; i++) {
+            const billet = $('#modele-billet').clone(true)
+            billet.css('display', 'inline-block')
+            billet.html(billet.html().replace('%valeur%', valeur))
+            if (valeur == 1) {
+                billet.html(billet.html().replace('Sous', 'Sou'))
+            }
+            $('#modele-billet').after(billet)
+        }
+        $('footer').addClass('no-print')
+    }
+}
