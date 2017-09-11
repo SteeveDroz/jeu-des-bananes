bc8beeeb3dfa1f38bbdaf5692416a6e801c77efa
Steeve Droz <steeve.droz@rpn.ch>
2017-09-11 16:04:26 +0200

    Sélection de la page active

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 $(() => {
     const header = $('<header>')
-    header.load('fragment/header.html')
+    header.load('fragment/header.html', selectCurrentPage)
     $('body').prepend(header)
 
     const footer = $('<footer>')
     footer.load('fragment/footer.html')
     $('body').append(footer)
 })
+
+const selectCurrentPage = function() {
+    let selected = false
+    $('menu a').each(function() {
+        if ($(this).attr('href').endsWith(location.href.split('/').pop())) {
+            $(this).addClass('selected')
+            selected = true
+        }
+    })
+
+    if (!selected) {
+        $('menu a').eq(0).addClass('selected')
+    }
+}
