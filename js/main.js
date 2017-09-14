d7a24e77b3d195e7c3cc1768cfd1d66d2df15db2
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-09-14 06:20:03 +0200

    Ajout du ruban

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-$(() => {
+$(function() {
     const header = $('<header>')
     header.load('fragment/header.html', selectCurrentPage)
     $('body').prepend(header)
-
     const footer = $('<footer>')
     footer.load('fragment/footer.html')
     $('body').append(footer)
+    $('body').append($('<a>', {
+        id: 'ribbon',
+        href: 'https://github.com/SteeveDroz/jeu-des-bananes',
+        target: '_blank',
+        text: 'Fork me on GitHub',
+        css: {
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            transform: 'translateY(107px) rotate(45deg)',
+            transformOrigin: '100% 100%',
+            background: '#ffe135',
+            color: 'darkslategray',
+            padding: '10px 50px',
+            width: 250,
+            textAlign: 'center',
+            textDecoration: 'none'
+        }
+    }))
 })
-
 const selectCurrentPage = function() {
     let selected = false
     $('menu a').each(function() {
@@ -16,7 +33,6 @@ const selectCurrentPage = function() {
             selected = true
         }
     })
-
     if (!selected) {
         $('menu a').eq(0).addClass('selected')
     }
