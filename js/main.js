a9b4633ae4abd0165ee4077906ff5f14a852c193
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-09-14 09:06:15 +0200

    Retrait du bandeau, indication en bas de page

--- js/main.js
+++ js/main.js
@@ -5,25 +5,6 @@ $(function() {
     const footer = $('<footer>')
     footer.load('fragment/footer.html')
     $('body').append(footer)
-    $('body').append($('<a>', {
-        id: 'ribbon',
-        href: 'https://github.com/SteeveDroz/jeu-des-bananes',
-        target: '_blank',
-        text: 'Fork me on GitHub',
-        css: {
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            transform: 'translateY(107px) rotate(45deg)',
-            transformOrigin: '100% 100%',
-            background: '#ffe135',
-            color: 'darkslategray',
-            padding: '10px 50px',
-            width: 250,
-            textAlign: 'center',
-            textDecoration: 'none'
-        }
-    }))
 })
 const selectCurrentPage = function() {
     let selected = false
